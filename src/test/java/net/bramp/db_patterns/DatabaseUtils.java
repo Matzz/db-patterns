ca160c3c5706551561ab76f7b127aa666b8171e5
Matzz <matzz@example.com>
2014-07-15 10:59:14 +0200

    test database config in separate file

--- src/test/java/net/bramp/db_patterns/DatabaseUtils.java
+++ src/test/java/net/bramp/db_patterns/DatabaseUtils.java
@@ -5,16 +5,25 @@ import javax.sql.DataSource;
 import com.google.common.base.Throwables;
 import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Properties;
 
 public class DatabaseUtils {
 	public static DataSource createDataSource() {
 		MysqlDataSource ds = new MysqlDataSource();
-		ds.setUser("root");
-		ds.setPassword("sP6prUCe");
-		ds.setServerName("localhost");
-		ds.setDatabaseName("db_patterns");
+		Properties prop;
+		try {
+			prop = getDatabaseProperties();
+			ds.setUser(prop.getProperty("user"));
+			ds.setPassword(prop.getProperty("password"));
+			ds.setServerName(prop.getProperty("serverName"));
+			ds.setDatabaseName(prop.getProperty("databaseName"));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 		return ds;
 	}
 
@@ -24,5 +33,21 @@ public class DatabaseUtils {
         } catch (UnknownHostException e) {
             throw Throwables.propagate(e);
         }
+    }
+    
+    protected static Properties getDatabaseProperties() throws IOException {
+    	Properties prop = new Properties();
+    	InputStream is = null;
+    	try {
+    		is = DatabaseUtils.class.getClassLoader().getResourceAsStream("database.properties");
+    	    prop.load(is);
+
+    	} 
+    	finally {
+    	    if(is!=null) {
+    	    	is.close();
+    	    }
+    	}
+    	return prop;
     }
 }
